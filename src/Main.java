c6ca1f955842e432670e4cce5378b2be6500b598
achub <a.chubakov005@gmail.com>
2023-10-29 18:47:32 +0100

    Nummer3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Nummer 2
@@ -12,6 +14,24 @@ public class Main {
             double y = Math.pow(Math.sin(x), 2) + x / 2;
             System.out.printf("%.2f\t| %.2f\n", x, y);
         }
+
+        //Nummer 3
+        Scanner scanner = new Scanner(System.in);
+
+        int input;
+        StringBuilder result = new StringBuilder();
+
+        do {
+            System.out.print("Введіть число (0 для завершення): ");
+            input = scanner.nextInt();
+
+            if (input >= 65 && input <= 90) { // Перевірка, чи код належить великим латинським літерам
+                char letter = (char) input; // Перетворення коду в символ
+                result.append(letter);
+            }
+        } while (input != 0);
+        System.out.println("Результат: " + result.toString());
+        scanner.close();
 
 
     }
